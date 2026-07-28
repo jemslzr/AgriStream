e5d9ba806a730477d06030cb576e2210fe1b77f4
jemslzr <salazarclarissejem@gmail.com>
2026-07-29 00:25:33 +0800

    ensure function with the jsdom environment

--- ayudastream-frontend/vite.config.ts
+++ ayudastream-frontend/vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // @ts-ignore
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
   },
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    target: 'esnext'
+  }
+});
